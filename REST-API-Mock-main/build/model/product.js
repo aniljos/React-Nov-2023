4877fdce5892d2f8dd5fea40a72cc84dc33a8dbc
aniljos <anil.jos@gmail.com>
2023-11-21 13:13:40 +0530

    Day 2 session 2

---
+++ REST-API-Mock-main/build/model/product.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+class Product {
+    constructor(id, name, price, description, imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+}
+exports.Product = Product;
--- the-awesome-app/src/components/FnCounter.tsx
+++ the-awesome-app/src/components/FnCounter.tsx
@@ -1,17 +1,29 @@
+import {ChangeEvent, useState, useRef} from 'react';
+
 function FnCounter(){
 
-    let counter = 0;
+    const [counter, setCounter] = useState(5);
+    const inputRef = useRef<HTMLInputElement>(null)
 
     function inc(){
         console.log("inc invoked..");
-        counter++;
-        console.log("counter", counter);
+        setCounter(counter + 1);
+        
     }
     const decr = () => {
 
         console.log("decr invoked");
-        counter--;
-        console.log("counter", counter);
+        setCounter(counter - 1);
+    }
+
+    function handleChange(evt: ChangeEvent<HTMLInputElement>){
+
+        const value = evt.target.value;
+        setCounter(Number(value));
+    }
+    function handleUpdate(){
+        console.log("inputRef", inputRef);
+        setCounter(Number( inputRef.current?.value));
     }
 
     return (
@@ -20,6 +32,15 @@ function FnCounter(){
             <div>
                 <button onClick={inc}>++</button>&nbsp;
                 <button onClick={decr}>--</button>
+            </div>
+            <div>
+                {/* controlled input */}
+                Counter: <input  type="number" value={counter} onChange={handleChange}/>
+            </div>
+            <div>
+                {/* uncontrolled input */}
+                Update Counter: <input ref={inputRef} type="number" defaultValue={counter}/> &nbsp;
+                <button onClick={handleUpdate}>Update</button>
             </div>
         </div>
     )
